ca360e82fdf67a25f6c9965a24711cb6e5d8e273
nahee kim <nahbee10@naver.com>
2019-04-29 00:10:37 -0400

    Updates

---
+++ precache-manifest.eae6f93e7d71b4efbedb245475d179ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "309b0388094ee4b8c371",
+    "url": "/static/css/main.ec7316fd.chunk.css"
+  },
+  {
+    "revision": "309b0388094ee4b8c371",
+    "url": "/static/js/main.61d2d3cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8bfe3e7b02a9667a3491",
+    "url": "/static/js/2.f4eeb69c.chunk.js"
+  },
+  {
+    "revision": "78d260b63ede671c0b99c90b48247b57",
+    "url": "/index.html"
+  }
+];
