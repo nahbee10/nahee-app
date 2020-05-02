dc424f6c204b123c9c282266520deb74c31fb7ed
nahee kim <nahbee10@naver.com>
2020-05-02 01:00:20 -0400

    Updates

---
+++ precache-manifest.a11db3d7354e2ac8af10ad6021ca6876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8860c3e7057be8824b1d",
+    "url": "/static/css/main.e7b29e1f.chunk.css"
+  },
+  {
+    "revision": "8860c3e7057be8824b1d",
+    "url": "/static/js/main.65afff0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad3c6986735f075b01d7",
+    "url": "/static/css/2.0ea46288.chunk.css"
+  },
+  {
+    "revision": "ad3c6986735f075b01d7",
+    "url": "/static/js/2.94caf5c4.chunk.js"
+  },
+  {
+    "revision": "8180e2d6e667482ddcadf546dfbca196",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39007f6145f2eb7758831ee68052841f.js"
+  "/precache-manifest.a11db3d7354e2ac8af10ad6021ca6876.js"
 );
 
 workbox.clientsClaim();
